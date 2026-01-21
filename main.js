64675d066d96bea9510e2c3da192db0349730ace
0x1B284004 <zenshi.on.top@gmail.com>
2026-01-21 20:40:14 +0100

    v6

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.169.0/build/three.module.js';
 
 document.body.addEventListener('mousemove', (e) => {
   document.body.style.setProperty('--cursorX', `${e.clientX}px`);
